8ac25d618d369fbfd55e9f400a572c39b56438ce
Martijn-Kalkman <kalkmanmartijn@gmail.com>
2023-06-14 09:58:59 +0200

    test-com

--- src/js/game.js
+++ src/js/game.js
@@ -10,7 +10,12 @@ export class Game extends Engine {
     }
 
     startGame() {
-
+        console.log("start de game!")
+        const fish = new Actor()
+        fish.graphics.use(Resources.Fish.toSprite())
+        fish.pos = new Vector(400, 300)
+        fish.vel = new Vector(-10,0)
+        this.add(fish)
     }
 }
 
